a704f4e499b27d3537dcdf28780d7fa3f635d44d
ishwar singh <icr7.dev@outlook.com>
2021-11-19 21:17:56 +0530

    fs-service: Spring initializer created

---
+++ fs-services/factsparrow/src/main/java/com/icr7/factsparrow/FactsparrowServicesApplication.java
@@ -0,0 +1,13 @@
+package com.icr7.factsparrow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactsparrowServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactsparrowServicesApplication.class, args);
+	}
+
+}
